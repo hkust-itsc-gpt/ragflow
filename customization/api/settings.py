02b5ac73e41fd17b30ba1904db081af9a5e005c3
Alex <alexcwchan@ust.hk>
2025-01-13 10:16:05 +0800

    Update settings.py

--- customization/api/settings.py
+++ customization/api/settings.py
@@ -137,8 +137,11 @@ def init_settings():
 
         if LLM_FACTORY:
             CHAT_MDL = default_llm[LLM_FACTORY]["chat_model"] + f"@{LLM_FACTORY}"
-            ASR_MDL = default_llm[LLM_FACTORY]["asr_model"] + f"@{LLM_FACTORY}"
-            IMAGE2TEXT_MDL = default_llm[LLM_FACTORY]["image2text_model"] + f"@{LLM_FACTORY}"
+#            ASR_MDL = default_llm[LLM_FACTORY]["asr_model"] + f"@{LLM_FACTORY}"
+#            IMAGE2TEXT_MDL = default_llm[LLM_FACTORY]["image2text_model"] + f"@{LLM_FACTORY}"
+        ### Add self-hosted LLama, match the model name as conf/llm_factories.json
+        ASR_MDL = default_llm["Tongyi-Qianwen"]["asr_model"] + "@Tongyi-Qianwen"
+        IMAGE2TEXT_MDL = default_llm["Tongyi-Qianwen"]["image2text_model"] + "@Tongyi-Qianwen"
         EMBEDDING_MDL = default_llm["BAAI"]["embedding_model"] + "@BAAI"
         RERANK_MDL = default_llm["BAAI"]["rerank_model"] + "@BAAI"
 
